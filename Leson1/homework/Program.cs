b044ad43fe9bb5aa160a3b40c2cbe91707d73e6e
yandreychenko <yandreychenko@ozon.ru>
2024-06-17 14:52:44 +0300

    add homework: leson1

---
+++ Leson1/homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычислить значение формулы ,где a, b, c, d — некоторые целые числа.
+// Результат вывести на экран.
+
+int a = 5;
+int b = 7;
+double c = 3;
+double d = 8;
+
+double quotient = (a * b)/(c + d);
+
+Console.WriteLine(quotient);
+
