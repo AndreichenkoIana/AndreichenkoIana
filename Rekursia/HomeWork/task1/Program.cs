9a8675767a613ae269a1f7d9588d4b1a1a9f5489
yandreychenko <yandreychenko@ozon.ru>
2024-07-10 12:00:10 +0300

    homework

---
+++ Rekursia/HomeWork/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+internal class Program
+{
+    static void Main()
+    {
+        int m = ReadInt("Введите первое число: ");
+        int n = ReadInt("Введите второе число: ");
+        if (m < n)
+        {
+            PrintNumber(m, n);
+        }
+        else
+        {
+            PrintNumber(n, m);
+        }
+    }
+
+    static void PrintNumber(int m, int n)
+    {
+        if (n < m)
+        {
+            return;
+        }
+
+        PrintNumber(m, n - 1);
+        Console.Write(n + " ");
+    }
+    static int ReadInt(string msg)
+    {
+        Console.Write(msg);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+}
---
+++ Rekursia/HomeWork/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(0, 1000);
+        }
+        PrintArrayReverse(numbers, numbers.Length - 1);
+    }
+
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " ");
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+}
