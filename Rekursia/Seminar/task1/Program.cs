f62c5b3763841d0331397ba13f6c7330c1fbe236
yandreychenko <yandreychenko@ozon.ru>
2024-07-10 11:18:33 +0300

    rerursia seminar

---
+++ Rekursia/Seminar/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using system;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void main()
+        {
+            int number = ReadInt("Введите число: ");
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(i + "  ");
+            }
+}
+
+        int ReadInt(string msg)
+        {
+            Console.Write(msg);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
