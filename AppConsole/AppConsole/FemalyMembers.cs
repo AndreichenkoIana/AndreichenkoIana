9c0ae68a1434813540951c80947f11d3ff5b0d86
yandreychenko <yandreychenko@ozon.ru>
2024-07-12 13:34:28 +0300

    Femaly

---
+++ AppConsole/AppConsole/FemalyMembers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsole
+{
+    public class FemalyMembers
+    {
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public FemalyMembers Husband { get; set; }
+        public FemalyMembers Wife { get; set; }
+        public string FullName { get; set; }
+        public FemalyMembers Mother { get; set; }
+        public FemalyMembers Father { get; set; }
+        public FemalyMembers? Partner { get; private set; }
+        public FemalyMembers? Parent1 { get; private set; }
+        public FemalyMembers? Parent2 { get; private set; }
+        public FemalyMembers? Children { get; private set; }
+
+    public FemalyMembers[] GetGrandMother()
+        {
+            return new FemalyMembers[] { Mother?.Mother, Father?.Mother };
+        }
+
+        public FemalyMembers[] GetGrandFather()
+        {
+            return new FemalyMembers[] { Mother?.Father, Father?.Father };
+        }
+        public FemalyMembers[] GetParent()
+        {
+            return new FemalyMembers[] { Mother, Father };
+        }    
+        public void SetPartner(FemalyMembers partner)
+        {
+            if (Partner != null) Partner.Partner = null;
+            partner.Partner = this;
+            Partner = partner;
+        }
+        public void SetChildren (FemalyMembers children)
+        {
+            if (children != null) children.Children = null;
+            children.Children = this;
+            Children = children;
+        }
+
+    }
+
+
+    public enum Gender { men, woman }
+
+
+}
---
+++ AppConsole/AppConsole/Program.cs
@@ -0,0 +1,75 @@
+﻿using AppConsole;
+using System.Runtime.Intrinsics.X86;
+public class Program
+{
+    public static void Main()
+    {
+        FemalyMembers GrandFatherMother = new FemalyMembers()
+        {
+            FullName = "Горобец Борис",
+            Age = 56,
+            Gender = Gender.men
+        };
+        FemalyMembers GrandFatherFather = new FemalyMembers()
+        {
+            FullName = "Андрейченко Александр",
+            Age = 75,
+            Gender = Gender.men
+        };
+        FemalyMembers GrandMotherMother = new FemalyMembers()
+        {
+            FullName = "Горобец Анжела",
+            Age = 55,
+            Gender = Gender.woman
+        };
+        FemalyMembers GrandMotherFather = new FemalyMembers()
+        {
+            FullName = "Андрейченко Надежда",
+            Age = 60,
+            Gender = Gender.woman
+        };
+        FemalyMembers Mother = new FemalyMembers()
+        {
+            FullName = "Андрейченко Яна",
+            Age = 28,
+            Gender = Gender.woman,
+            Mother = GrandMotherMother,
+            Father = GrandFatherMother
+            
+        };
+        FemalyMembers Father = new FemalyMembers()
+        {
+            FullName = "Андрейченко Марк",
+            Age = 34,
+            Gender = Gender.men,
+            Mother = GrandMotherFather,
+            Father = GrandFatherFather
+        };
+        FemalyMembers Daughter = new FemalyMembers()
+        {
+            FullName = "Андрейченко Елизавета",
+            Age = 4,
+            Gender = Gender.woman,
+            Mother = Mother,
+            Father = Father
+        };
+        Mother.SetPartner(Father);
+        Father.SetPartner(Mother);
+        Father.SetChildren(Daughter);
+        Mother.SetChildren(Daughter);
+        var Parent1 = Father.GetParent();
+        var Parent2 = Mother.GetParent();
+        Console.WriteLine($"Дерево человека: {Father.FullName}");
+        Console.WriteLine("____________________");
+        Console.WriteLine($"Родители {Father.FullName}: ");
+        Console.WriteLine($"Мама: {Parent1[0].FullName}");
+        Console.WriteLine($"Папа: {Parent1[1].FullName}");
+        Console.WriteLine("____________________");
+        Console.WriteLine($"Жена: {Father.Partner.FullName}");
+        Console.WriteLine($"Родители {Father.Partner.FullName}: ");
+        Console.WriteLine($"Мама: {Parent2[0].FullName}");
+        Console.WriteLine($"Папа: {Parent2[1].FullName}");
+        Console.WriteLine("____________________");
+        Console.WriteLine($"Дочь: {Father.Children.FullName}");
+    }
+}
