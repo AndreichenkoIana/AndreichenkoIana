6d07c44290f8f60b40f5bc7ad143d9997e843b4e
yandreychenko <yandreychenko@ozon.ru>
2024-07-12 16:55:47 +0300

    2

---
+++ Разработка приложения на C#/Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long longNumber = 1234567890123456789;
+        int intNumber = 123456789;
+        byte byteNumber = 200;
+
+        string longBits = Convert.ToString(longNumber, 2);
+        string intBits = Convert.ToString(intNumber, 2);
+        string byteBits = Convert.ToString(byteNumber, 2);
+
+        Console.WriteLine($"Биты для long: {longBits}");
+        Console.WriteLine($"Биты для int: {intBits}");
+        Console.WriteLine($"Биты для byte: {byteBits}");
+    }
+}
