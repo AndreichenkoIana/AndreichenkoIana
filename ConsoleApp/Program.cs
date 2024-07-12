a8926b9c69c04d3d9acb7eb77b45a7ba5acba6ab
yandreychenko <yandreychenko@ozon.ru>
2024-07-12 13:33:09 +0300

    Femaly

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿public class FamalyMembers
+{
+    int Age { get; set; }
+    Gender Gender { get; set; }
+    string FullName { get; set; }
+    FamalyMembers Mother { get; set; }
+    FamalyMembers Father { get; set; }
+
+    public FamalyMembers[] GetGrandMother()
+    {
+        return new FamalyMembers[] { Mother.Mother, Father.Mother };
+    }
+
+    public FamalyMembers[] GetGrandFather()
+    {
+        return new FamalyMembers[] { Mother.Father, Father.Father };
+    }
+}
+
+
+enum Gender { men, woman}
