9cbf18f48e8cf1972f0e55ae4e53fb08693273db
yandreychenko <yandreychenko@ozon.ru>
2024-08-14 17:16:01 +0300

    task3

---
+++ Разработка приложения на C#/Task3/Task3/Labirynth.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Labirynth
+{
+    private static int[] rowDirections = { -1, 1, 0, 0 };
+    private static int[] colDirections = { 0, 0, -1, 1 };
+
+    public static int CountExits(int startI, int startJ, int[,] labirynth)
+    {
+        int rows = labirynth.GetLength(0);
+        int cols = labirynth.GetLength(1);
+        bool[,] visited = new bool[rows, cols];
+        return DFS(startI, startJ, labirynth, visited, rows, cols);
+    }
+
+    private static int DFS(int i, int j, int[,] labirynth, bool[,] visited, int rows, int cols)
+    {
+        // Проверяем, находится ли текущая позиция за пределами лабиринта или является стеной
+        if (i < 0 || i >= rows || j < 0 || j >= cols || labirynth[i, j] == 1 || visited[i, j])
+        {
+            return 0;
+        }
+
+        // Если текущая позиция на границе лабиринта и является проходимой (значение 0), считаем это выходом
+        if ((i == 0 || i == rows - 1 || j == 0 || j == cols - 1) && labirynth[i, j] == 0)
+        {
+            return 1;
+        }
+
+        // Отмечаем текущую ячейку как посещенную
+        visited[i, j] = true;
+
+        int exits = 0;
+
+        // Пытаемся двигаться в четырех направлениях: вверх, вниз, влево и вправо
+        for (int direction = 0; direction < 4; direction++)
+        {
+            int newRow = i + rowDirections[direction];
+            int newCol = j + colDirections[direction];
+            exits += DFS(newRow, newCol, labirynth, visited, rows, cols);
+        }
+
+        return exits;
+    }
+}
---
+++ Разработка приложения на C#/Task3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static Labirynth;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] labirynth1 = new int[,]
+        {
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 0, 0, 0, 0, 0, 1 },
+            {1, 0, 1, 1, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0, 0 },
+            {1, 1, 0, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 },
+            {1, 1, 1, 0, 1, 1, 1 }
+        };
+
+        int startI = 3;
+        int startJ = 0;
+
+        int exitCount = CountExits(startI, startJ, labirynth1);
+        Console.WriteLine($"Количество выходов в лабиринте: {exitCount}");
+    }
+}
