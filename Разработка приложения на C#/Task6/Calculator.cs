9c69f465ceceafc4bcc68c00b7df932750ac9da7
yandreychenko <yandreychenko@ozon.ru>
2024-08-16 12:32:48 +0300

    Task6

---
+++ Разработка приложения на C#/Task6/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Calculator
+{
+    // Перегрузка операторов для работы с целыми и дробными числами
+    public static double operator +(Calculator c, double value)
+    {
+        return c.Value + value;
+    }
+
+    public static double operator -(Calculator c, double value)
+    {
+        return c.Value - value;
+    }
+
+    public static double operator *(Calculator c, double value)
+    {
+        return c.Value * value;
+    }
+
+    public static double operator /(Calculator c, double value)
+    {
+        if (value == 0)
+            throw new DivideByZeroException("Деление на ноль");
+        return c.Value / value;
+    }
+
+    public double Value { get; set; }
+
+    public Calculator(double initialValue)
+    {
+        Value = initialValue;
+    }
+}
---
+++ Разработка приложения на C#/Task6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Calculator calc = new Calculator(num1);
+
+        Console.WriteLine("Введите оператор (+, -, *, /):");
+        char op = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        try
+        {
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = calc + num2;
+                    break;
+                case '-':
+                    result = calc - num2;
+                    break;
+                case '*':
+                    result = calc * num2;
+                    break;
+                case '/':
+                    result = calc / num2;
+                    break;
+                default:
+                    Console.WriteLine("Некорректный оператор!");
+                    return;
+            }
+
+            Console.WriteLine($"Результат: {result}");
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Ошибка: {e.Message}");
+        }
+    }
+}
