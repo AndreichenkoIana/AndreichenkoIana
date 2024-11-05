ff624912a4b4dcb55eb2833c474dca8be8e6a582
yandreychenko <yandreychenko@ozon.ru>
2024-11-05 18:58:20 +0300

    Seminar2

---
+++ SettingAppCSHARP/Seminar/Seminar2/ConsoleApp/ConsoleApp/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class Client
+    {
+        public static void SendMsg(string name)
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 16874);
+            UdpClient udpClient = new UdpClient();
+
+            while (true)
+            {
+                Console.WriteLine("Введите сообщение (или 'Exit' для выхода):");
+                string userInput = Console.ReadLine();
+
+                if (userInput.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Завершение работы клиента.");
+                    break;
+                }
+
+                Message msg = new Message(name, userInput);
+                string responseMsgJs = msg.ToJson();
+                byte[] responseData = Encoding.UTF8.GetBytes(responseMsgJs);
+                udpClient.Send(responseData, ep);
+
+                byte[] answerData = udpClient.Receive(ref ep);
+                string answerMsgJs = Encoding.UTF8.GetString(answerData);
+                Message answerMsg = Message.FromJson(answerMsgJs);
+                Console.WriteLine(answerMsg.ToString());
+            }
+
+            udpClient.Close();
+        }
+    }
+}
---
+++ SettingAppCSHARP/Seminar/Seminar2/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Server.AcceptMsg();
+            }
+            else
+            {
+                new Thread(() =>
+                {
+                    Client.SendMsg($"{args[0]}");
+                }).Start();
+            }
+        }
+    }
+}
