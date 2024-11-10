7dc4002fefffc3725fca7d220964589c0ad4ce1d
yandreychenko <yandreychenko@ozon.ru>
2024-11-10 15:21:42 +0300

    Sem3

---
+++ SettingAppCSHARP/Seminar/Seminar3/HomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                await Server.AcceptMsg();
+            }
+            else
+            {
+                await Client.SendMsg($"{args[0]}");
+            }
+        }
+    }
+}
---
+++ SettingAppCSHARP/Seminar/Seminar3/HomeWork/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace HomeWork
+{
+    internal class Server
+    {
+        private static bool isRunning = true;
+        private static CancellationTokenSource cancellationToken = new CancellationTokenSource();
+        public static async Task AcceptMsg()
+        {
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 0);
+            UdpClient udpClient = new UdpClient(16874);
+            Console.WriteLine("Сервер ожидает сообщение. Нажмите любую клавишу для завершения работы.");
+
+            try
+            {
+                while (isRunning)
+                {
+                    if (udpClient.Available > 0)
+                    {
+                        byte[] buffer = udpClient.Receive(ref ep);
+                        string data = Encoding.UTF8.GetString(buffer);
+
+                        // Проверка на получение команды "Exit"
+                        if (data.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+                        {
+                            cancellationToken.Cancel();
+                        }
+
+                        await Task.Run(async () =>
+                        {
+                            Message msg = Message.FromJson(data);
+                            System.Console.WriteLine(msg.ToString());
+
+                            Message responseMsg = new Message("Server", "Message accept on server!");
+                            string responseMsgJs = responseMsg.ToJson();
+                            byte[] responseDate = Encoding.UTF8.GetBytes(responseMsgJs);
+                            await udpClient.SendAsync(responseDate, ep);
+                        });
+                    }
+                }
+
+                udpClient.Close();
+                Console.WriteLine("Сервер остановлен.");
+            }
+            catch
+            {
+                Console.WriteLine("Сlient has stopped execution");
+            }
+        }
+
+    }
+}
