b6066767c3383f254be5ccbdada47a0e4f74efdf
yandreychenko <yandreychenko@ozon.ru>
2024-11-13 08:57:22 +0300

    upd

---
+++ Исключения в программировании и их обработка/Seminar_1/Work1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Seminar1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                int[] array1 = { 5, 10, 15 };
+                int[] array2 = { 3, 6, 9, 10};
+
+                int[] resultArray = ArrayDifference.SubtractArrays(array1, array2);
+
+                Console.WriteLine("Результирующий массив:");
+                foreach (int value in resultArray)
+                {
+                    Console.Write(value + " ");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
