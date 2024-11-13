e72484292d0445e1b85f1306ad64c7497bd9ad61
yandreychenko <yandreychenko@ozon.ru>
2024-11-13 09:02:10 +0300

    Sem2_Work1

---
+++ Исключения в программировании и их обработка/Seminar2/Work2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Work2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                float userInput = GetFloatFromUser();
+                Console.WriteLine($"Вы ввели число: {userInput}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static float GetFloatFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Пожалуйста, введите дробное число: ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    throw new ArgumentException("Пустые строки вводить нельзя.");
+                }
+
+                if (float.TryParse(input, out float result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                }
+            }
+        }
+    }
+}
