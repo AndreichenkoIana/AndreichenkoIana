d215d5c625ce25d614e306d5bddce7fdafec98ff
yandreychenko <yandreychenko@ozon.ru>
2024-11-14 11:13:57 +0300

    Seminar3

---
+++ Исключения в программировании и их обработка/Seminar3/Work/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Work;
+
+class UserDataApp
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите данные в формате: Фамилия Имя Отчество Дата_рождения Номер_телефона Пол");
+        string input = Console.ReadLine();
+        ValidateMessage.Validate(input);
+    }
+}
---
+++ Исключения в программировании и их обработка/Seminar3/Work/WriteToFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    internal class WriteToFile
+    {
+        internal static void WriteFile(string lastName, string firstName, string patronymic, DateTime birthDate, long phoneNumber, char gender)
+        {
+            // Удаление недопустимых символов из имени файла
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            string safeLastName = new string(lastName.Where(c => !invalidChars.Contains(c)).ToArray());
+
+            string fileName = safeLastName + ".txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName, true))
+                {
+                    writer.WriteLine($"{lastName} {firstName} {patronymic} {birthDate:dd.MM.yyyy} {phoneNumber} {gender}");
+                    Console.WriteLine("Данные успешно записаны в файл: " + fileName);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ошибка записи в файл: " + e.Message);
+            }
+        }
+    }
+}
