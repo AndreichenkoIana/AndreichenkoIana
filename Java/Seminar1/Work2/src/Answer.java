65c4c82f154e782c83d33ed75fa0eec9f07e0691
yandreychenko <yandreychenko@ozon.ru>
2024-12-04 09:33:29 +0300

    W2

---
+++ Java/Seminar1/Work2/src/Answer.java
@@ -0,0 +1,11 @@
+public class Answer
+{
+    public static void printEvenNums()
+    {
+        for (int i = 2; i <= 100; i += 2)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
