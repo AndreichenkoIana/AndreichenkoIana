a77cd905884cb77a54a64beb5adab76ef8bb8c11
yandreychenko <yandreychenko@ozon.ru>
2024-12-25 09:34:47 +0300

    Sem5

---
+++ Java/Seminar5/Work2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BrowserHistory browserHistory = new BrowserHistory();
+        browserHistory.visitSite("google.com");
+        browserHistory.visitSite("stackoverflow.com");
+        browserHistory.visitSite("github.com");
+        System.out.println(browserHistory.back(1));
+        System.out.println(browserHistory.getHistory());
+    }
+}
